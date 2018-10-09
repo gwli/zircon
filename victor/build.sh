89373ffafd812c36f9a2c85f748b584121fe7902
victor <vili@nvidia.com>
2018-10-09 12:06:45 +0800

    build log

--- victor/build.sh
+++ victor/build.sh
@@ -1,8 +1,18 @@
 TOP=`pwd`/..
 
 cd $TOP
-#sudo true
-#sudo apt-get install -y texinfo libglib2.0-dev autoconf libtool bison libsdl-dev build-essential
-# ./scripts/download-prebuilt;
+
+function test1() {
+sudo true
+sudo apt-get install -y texinfo libglib2.0-dev autoconf libtool bison libsdl-dev build-essential
+./scripts/download-prebuilt;
 make clean
 make -j32 USE_CLANG=true QUIET=0 x64 2>&1 |  tee build.log
+}
+
+function test2() {
+ time ./scripts/build-zircon -C -v -a x64 2>&1 | tee build.log
+}
+
+test2
+
